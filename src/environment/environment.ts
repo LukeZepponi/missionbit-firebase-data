c1fa060e17a7c4cfcbee30bc7d13dfe0b0018070
Luke Zepponi <lukezepponi@gmail.com>
2018-10-25 18:07:31 -0700

    first commit

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+      firebase: {
+        apiKey: "AIzaSyBuly5mrJ9R6_Ir3zCLVpEL2lamTg_uys0",
+        authDomain: "lukez14-218a9.firebaseapp.com",
+        databaseURL: "https://lukez14-218a9.firebaseio.com",
+        projectId: "lukez14-218a9",
+        storageBucket: "lukez14-218a9.appspot.com",
+        messagingSenderId: "298599191307"
+    }
+  };
